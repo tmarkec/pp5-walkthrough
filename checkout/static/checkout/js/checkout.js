c839f3b079dd5d20cfb302e753de2e7b35f66ef8
tmarkec <>
2023-04-11 14:47:19 +0000

    add keys to cloud

--- checkout/static/checkout/js/checkout.js
+++ checkout/static/checkout/js/checkout.js
@@ -1,5 +1,6 @@
 var stripe = Stripe('pk_test_51MvgJBJHKVQkHL7vZp6wh1IDXiDu8QAdCTIFcHqsGA8pNaoI1N3GdkTjQeYdVuiHdAw0D4j9N7Uv2fL7I4lQGDFK00aQNgiFB0');
 var elements = stripe.elements();
+
 var style = {
     base: {
         color: '#000',
@@ -17,4 +18,14 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
-  
+
+
+// handle realtime validation
+card.on('change', ({error}) => {
+    let displayError = document.getElementById('card-errors');
+    if (error) {
+      displayError.textContent = error.message;
+    } else {
+      displayError.textContent = '';
+    }
+  });
