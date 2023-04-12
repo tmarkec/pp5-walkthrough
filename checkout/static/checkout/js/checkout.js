99796ef18897427cf68dffff42c01cd3a55e9c17
tmarkec <>
2023-04-12 10:17:31 +0000

    add checkout succes html

--- checkout/static/checkout/js/checkout.js
+++ checkout/static/checkout/js/checkout.js
@@ -1,26 +1,47 @@
+// var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+// var clientSecret = $('#id_client_secret').text().slice(1, -1);
+// var stripe = Stripe(stripePublicKey);
+// var elements = stripe.elements();
+
+// var style = {
+//     base: {
+//         color: '#000',
+//         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+//         fontSmoothing: 'antialiased',
+//         fontSize: '16px',
+//         '::placeholder': {
+//             color: '#aab7c4'
+//         }
+//     },
+//     invalid: {
+//         color: '#dc3545',
+//         iconColor: '#dc3545'
+//     }
+// };
+// var card = elements.create('card', {style: style});
+// card.mount('#card-element');
+
+
+// // handle realtime validation
+// card.on('change', ({error}) => {
+//     let displayError = document.getElementById('card-errors');
+//     if (error) {
+//       displayError.textContent = error.message;
+//     } else {
+//       displayError.textContent = '';
+//     }
+//   });
 var stripe = Stripe('pk_test_51MvgJBJHKVQkHL7vZp6wh1IDXiDu8QAdCTIFcHqsGA8pNaoI1N3GdkTjQeYdVuiHdAw0D4j9N7Uv2fL7I4lQGDFK00aQNgiFB0');
 var elements = stripe.elements();
-
+var elements = stripe.elements();
 var style = {
-    base: {
-        color: '#000',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-            color: '#aab7c4'
-        }
-    },
-    invalid: {
-        color: '#dc3545',
-        iconColor: '#dc3545'
-    }
+  base: {
+    color: "#32325d",
+  }
 };
-var card = elements.create('card', {style: style});
-card.mount('#card-element');
 
-
-// handle realtime validation
+var card = elements.create("card", { style: style });
+card.mount("#card-element");
 card.on('change', ({error}) => {
     let displayError = document.getElementById('card-errors');
     if (error) {
@@ -29,3 +50,30 @@ card.on('change', ({error}) => {
       displayError.textContent = '';
     }
   });
+
+var form = document.getElementById('order-form');
+
+form.addEventListener('submit', function(ev) {
+  ev.preventDefault();
+  card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+
+  stripe.confirmCardPayment(clientSecret, {
+    payment_method: {
+      card: card,
+    
+    }
+  }).then(function(result) {
+    if (result.error) {
+ 
+      console.log(result.error.message);
+      card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+    } else {
+    
+      if (result.paymentIntent.status === 'succeeded') {
+        form.submit();
+      }
+    }
+  });
+});
