b69c571de942c0ee310a04764ae9b23b8ca07a3f
tmarkec <>
2023-04-13 13:22:54 +0000

    add widgets for image field

--- profiles/views.py
+++ profiles/views.py
@@ -4,7 +4,9 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 from checkout.models import Order
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -28,6 +30,7 @@ def profile(request):
     }
 
     return render(request, template, context)
+
 
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
